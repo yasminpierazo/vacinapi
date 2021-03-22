aaa178c83e1f404a1b8db8e83ec012664e6173df
yasminpierazo <ypierazo@gmail.com>
2021-03-22 10:05:55 -0300

    Update MainController.java

--- src/main/java/com/api/vacinas/controllers/MainController.java
+++ src/main/java/com/api/vacinas/controllers/MainController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // Indica que a classe é um controller
+@CrossOrigin
 @RequestMapping(path = "/cadastros")
 public class MainController {
 	@Autowired
