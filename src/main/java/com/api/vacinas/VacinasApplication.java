0d66eac98c66e269015f5a24a1d1ae43a63cc457
yasminpierazo <ypierazo@gmail.com>
2021-03-22 10:06:17 -0300

    Update VacinasApplication.java

--- src/main/java/com/api/vacinas/VacinasApplication.java
+++ src/main/java/com/api/vacinas/VacinasApplication.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableWebMvc
 public class VacinasApplication {
 
 	public static void main(String[] args) {
