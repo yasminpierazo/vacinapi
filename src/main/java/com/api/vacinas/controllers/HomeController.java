31293c3fd384870fbbf89ecd16be902875bd794b
yasminpierazo <ypierazo@gmail.com>
2021-03-22 09:51:37 -0300

    Update HomeController.java

--- src/main/java/com/api/vacinas/controllers/HomeController.java
+++ src/main/java/com/api/vacinas/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index() {
         return "index";
     }
